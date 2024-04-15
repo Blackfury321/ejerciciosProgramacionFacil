74f50afe2f724c159512d2414918378ca54a40d4
blackfury <113058714+Blackfury321@users.noreply.github.com>
2024-04-15 20:45:18 +0200

    Update calculo_area_cuadrado.py

---
+++ calculo_area_cuadrado.py
@@ -0,0 +1,8 @@
+# solicitar la longitud de un lado 
+longitud = float(input("distancia del lado = "))
+# calcular el area
+area = longitud * longitud
+perimetro = longitud * 4
+# mostrar el resiltado 
+print(f"El area del cuadrado es de: {area} y su perimetro es de {perimetro}")
+
